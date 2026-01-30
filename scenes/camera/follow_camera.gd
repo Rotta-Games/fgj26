d2ce1bf8e224f94d78fcf02280f56957d78d92c0
Joni Louhela <louhela.joni@gmail.com>
2026-01-30 20:56:25 +0200

    Fix camera

--- scenes/camera/follow_camera.gd
+++ scenes/camera/follow_camera.gd
@@ -1,8 +1,8 @@
 extends Camera2D
 
 
-@onready var target: Node2D = $"../Player"
-@onready var viewport = $"../../../SubViewportContainer"
+@export var target: Node2D
+@export var viewport : SubViewportContainer
 
 var actual_cam_pos: Vector2
 
@@ -13,8 +13,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if target:
 		actual_cam_pos = actual_cam_pos.lerp(target.global_position, delta * 3)
-
-		var cam_subpixel_offset = Vector2((actual_cam_pos.round() - actual_cam_pos).x, global_position.y)
+		var cam_subpixel_offset = Vector2((actual_cam_pos.round() - actual_cam_pos).x, 0.0)
 
 		viewport.material.set_shader_parameter("cam_offset", cam_subpixel_offset)
 
