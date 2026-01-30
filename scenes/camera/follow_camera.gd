c04c320b8a8932377edf590f4548928b4e01fb4e
Joni Louhela <louhela.joni@gmail.com>
2026-01-30 22:24:31 +0200

    Add camera bounds signal

--- scenes/camera/follow_camera.gd
+++ scenes/camera/follow_camera.gd
@@ -4,17 +4,21 @@ extends Camera2D
 @export var target: Node2D
 @export var viewport : SubViewportContainer
 
-var actual_cam_pos: Vector2
+var _actual_cam_pos: Vector2
 
 func _ready() -> void:
 	pass
 
-
 func _process(delta: float) -> void:
-	if target:
-		actual_cam_pos = actual_cam_pos.lerp(target.global_position, delta * 3)
-		var cam_subpixel_offset = Vector2((actual_cam_pos.round() - actual_cam_pos).x, 0.0)
+	if not target:
+		return
+		
+	_actual_cam_pos = _actual_cam_pos.lerp(target.global_position, delta * 3)
+	var cam_subpixel_offset = Vector2((_actual_cam_pos.round() - _actual_cam_pos).x, 0.0)
+	viewport.material.set_shader_parameter("cam_offset", cam_subpixel_offset)
+	global_position.x = _actual_cam_pos.round().x
 
-		viewport.material.set_shader_parameter("cam_offset", cam_subpixel_offset)
-
-		global_position.x = actual_cam_pos.round().x
+func _on_stage_camera_limit_changed(left: int, right: int) -> void:
+	limit_right = right
+	limit_left = left
+	print(limit_right)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var player_stats: PlayerStats
+@export var camera: Camera2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
