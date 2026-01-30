0c16dfe7871c677559b3cd4e6571b5e8a12dc98b
Teemu Risikko <teemu.risikko@gmail.com>
2026-01-30 20:36:07 +0200

    Initial commit

---
+++ scripts/follow_camera.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+
+@onready var target: Node2D = $"../Player"
+@onready var viewport = $"../../../SubViewportContainer"
+
+var actual_cam_pos: Vector2
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	if target:
+		actual_cam_pos = actual_cam_pos.lerp(target.global_position, delta * 3)
+
+		var cam_subpixel_offset = Vector2((actual_cam_pos.round() - actual_cam_pos).x, global_position.y)
+
+		viewport.material.set_shader_parameter("cam_offset", cam_subpixel_offset)
+
+		global_position.x = actual_cam_pos.round().x
