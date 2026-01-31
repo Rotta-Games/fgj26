4ac9050ee90bb23ecd1284a1905a09297aad5eed
Pete Sevander <pete.sevander@gmail.com>
2026-01-31 08:19:44 +0000

    esc to quit

--- scenes/stage/stage.gd
+++ scenes/stage/stage.gd
@@ -17,3 +17,8 @@ func _set_block(block: StageBlock) -> void:
 	_current_camera_limit += block.length
 	camera_limit_changed.emit(prev_camera_limit, _current_camera_limit)
 	
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == Key.KEY_ESCAPE:
+			get_tree().quit()
