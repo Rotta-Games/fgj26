1272da2f1af71c71c6a1358482ec5ca9037d2ba9
Joni Louhela <louhela.joni@gmail.com>
2026-01-31 18:24:48 +0200

    Arrow export variables

--- scenes/game_ui/arrow.gd
+++ scenes/game_ui/arrow.gd
@@ -1,7 +1,7 @@
 extends TextureRect
 
-const FLICKER_TIME : float = 0.33
-const FLICKER_TIMES : int = 5
+@export var flicker_duration : float = 0.33
+@export var flicker_times : int = 5
 
 var _first_time : bool = true
 
@@ -9,8 +9,8 @@ func _on_stage_camera_right_limit_changed(right_limit: int) -> void:
 	if _first_time:
 		_first_time = false
 		return
-	for i in range(FLICKER_TIMES):
+	for i in range(flicker_times):
 		self.show()
-		await get_tree().create_timer(FLICKER_TIME).timeout
+		await get_tree().create_timer(flicker_duration).timeout
 		self.hide()
-		await get_tree().create_timer(FLICKER_TIME).timeout
+		await get_tree().create_timer(flicker_duration).timeout
