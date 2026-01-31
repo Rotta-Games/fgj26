b3e866a723e3457cebcb489146acf76af806d0cd
Juho Myllylahti <juho.myllylahti@iki.fi>
2026-01-31 15:16:36 +0200

    Add initial game music

--- scripts/autoloads/audio_manager.gd
+++ scripts/autoloads/audio_manager.gd
@@ -1,11 +1,14 @@
 extends Node
 
+var menu_music = preload("res://assets/music/ggj26-145-sid-main-loop-01.mp3")
 @onready var music_player: AudioStreamPlayer = AudioStreamPlayer.new()
 
 func _ready():
 	add_child(music_player)
 	music_player.bus = "Music"
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	play_music(menu_music)
 
 func play_music(stream: AudioStream):
 	if music_player.stream == stream and music_player.playing:
