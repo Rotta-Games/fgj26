1887937b8c3c23f4436dfa3791db8481c67dc109
Teemu Risikko <teemu.risikko@gmail.com>
2026-01-31 15:53:41 +0200

    Switch to pixel-perfect mode

--- scenes/camera/follow_camera.gd
+++ scenes/camera/follow_camera.gd
@@ -3,7 +3,6 @@ extends Camera2D
 @export var target: Node2D
 @export var viewport : SubViewportContainer
 
-var _actual_cam_pos: Vector2
 var _checkpoint_reached : bool = false
 var _checkpoint_limit: int = 999999999
 
@@ -25,13 +24,7 @@ func _init_camera() -> void:
 func _process(delta: float) -> void:
 	if not target:
 		return
-		
-	# TODO ota käyttöön joskus
-	#_actual_cam_pos = _actual_cam_pos.lerp(target.global_position, delta * 3)
-	#var cam_subpixel_offset = Vector2((_actual_cam_pos.round() - _actual_cam_pos).x, 0.0)
-	#viewport.material.set_shader_parameter("cam_offset", cam_subpixel_offset)
-	#global_position.x = _actual_cam_pos.round().x - _camera_left_offset
-	
+
 	var canvas_transform = get_viewport().get_canvas_transform()
 	var global_screen_left = -canvas_transform.origin.x
 	limit_left = global_screen_left
