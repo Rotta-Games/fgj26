8b0176aff9fdd45be4163f4cee1742c489b7b904
Joonas Reinikka <joonas.reinikka@gmail.com>
2026-02-01 13:59:09 +0200

    Add GAS GAS GAS!

---
+++ scenes/boss/gas.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+@onready var hit_area = $Area2D
+@onready var attack_timer = $AttackTimer
+@onready var alive_timer = $AliveTimer
+@onready var sprite = $AnimatedSprite2D
+
+# Props
+var attack_delay: float = 0.5
+var attack_damage: int = 10
+var alive_time: float = 8.0
+
+# States
+var damage_dealt = false
+
+func _ready() -> void:
+	sprite.play("default")
+	alive_timer.start(alive_time)
+
+func _physics_process(_delta: float) -> void:
+	if !damage_dealt:
+		for area in hit_area.get_overlapping_areas():
+				if "PlayerHitbox" in area.get_groups():
+					var typed_area = area as Area2D
+					var player := typed_area.get_parent()
+					if "Player" in player.get_groups() && player.has_method("hurt"):
+						player.hurt(attack_damage)
+						damage_dealt = true
+						attack_timer.start(attack_delay)
+						
+
+func _disable_all_collisions() -> void:
+	$Area2D/CollisionShape2D.set_deferred("disabled", true)
+
+
+func _on_attack_timer_timeout():
+	damage_dealt = false
+
+
+func _on_alive_timer_timeout():
+	queue_free()
