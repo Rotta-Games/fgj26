81cee79504a1914355cb933789f75347c83ded4d
Pete Sevander <pete.sevander@gmail.com>
2026-02-01 12:15:10 +0000

    fugly help menu

--- scenes/mainmenu.gd
+++ scenes/mainmenu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$HelpCont.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +22,8 @@ func _on_quit_button_button_up() -> void:
 
 func _on_start_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_help_button_button_up() -> void:
+	$HelpCont.visible = !$HelpCont.visible
+
