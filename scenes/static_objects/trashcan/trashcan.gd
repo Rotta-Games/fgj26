3d177bd80b93eacda648b7f8cf9227bb7709d52a
Joonas Reinikka <joonas.reinikka@gmail.com>
2026-02-01 13:41:27 +0200

    Add animation to trash can

--- scenes/static_objects/trashcan/trashcan.gd
+++ scenes/static_objects/trashcan/trashcan.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+@onready var animation_player = $AnimationPlayer
+
 @export var data: StaticObject
 
 var health: int
@@ -10,15 +12,18 @@ func _ready() -> void:
 	drop = data.drop
 	
 func hurt(amount: int):
+	animation_player.play("hurt")
 	health -= amount
 	
 	if (health <= 0):
 		_destroy()
 
 func _destroy() -> void:
+	animation_player.play("dead")
 	if drop:
 		var drop_instance := drop.instantiate()
 		if drop_instance is Node2D:
 			drop_instance.global_position = global_position
 		get_tree().current_scene.add_child(drop_instance)
+	await animation_player.animation_finished
 	queue_free()
